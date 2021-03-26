9839f3acb14a187aa55a0608d5a6098d5bb0797f
Jose Rueda Rueda <jose.rueda.rueda@ipp.mpg.de>
2021-03-26 19:58:21 +0100

    first implementation of time slider

--- Lib/LibVideoFiles.py
+++ Lib/LibVideoFiles.py
@@ -11,19 +11,18 @@ import re
 import warnings
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk                       # To open UI windows
 import LibPlotting as ssplt
 import LibMap as ssmap
 import LibPaths as p
 import LibUtilities as ssutilities
 import LibIO as ssio
+import LibVideoGUI as ssvidGUI             # For GUI element
 from LibMachine import machine
 from version_suite import version
 from scipy.io import netcdf                # To export remap data
 from scipy import ndimage                  # To filter the images
-from skimage import io                     # To load png images
-import tkinter as tk                       # To open UI windows
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+from skimage import io                     # To load images
 from tqdm import tqdm                      # For waitbars
 pa = p.Path(machine)
 del p
@@ -1698,8 +1697,12 @@ class Video:
 
     def plot_frames_slider(self):
         "Plot the frames with a slider which allows to select the desired time"
+        text = 'Press TAB until the time slider is highlighted in red.'\
+            + ' Once that happend, you can move the time with the arrows'\
+            + ' of the keyboard, frame by frame'
+        print(text)
         root = tk.Tk()
-        ApplicationShowVid(root, self.exp_dat)
+        ssvidGUI.ApplicationShowVid(root, self.exp_dat)
         root.mainloop()
         root.destroy()
 
@@ -2209,47 +2212,3 @@ class Video:
             except KeyError:
                 print('Bits info not present in the video object')
         return
-
-
-class ApplicationShowVid:
-    """Class to show the frames"""
-
-    def __init__(self, master, data):
-        """
-        Create the window with the sliders
-
-        @param master: Tk() opened
-        @param data: the dictionary of experimental frames or remapped ones
-        """
-        # Save here the data
-        self.data = data
-        # Create a container
-        frame = tk.Frame(master)
-        # Create the time slider
-        t = data['tframes']
-        print(t.shape)
-        print(t.size)
-        dt = t[1] - t[0]
-        self.tSlider = tk.Scale(master, from_=t[0], to=t[-1], resolution=dt,
-                                command=self.plot_frame)
-        self.tSlider.pack(side='right')
-        # create the quit button
-        self.qButton = tk.Button(master, text="Quit", command=master.quit)
-        self.qButton.pack(side='bottom')
-
-        fig = Figure()
-        ax = fig.add_subplot(111)
-        self.image = ax.imshow(data['frames'][:, :, 0].squeeze(),
-                               origin='lower')
-        self.canvas = FigureCanvasTkAgg(fig, master=master)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
-        frame.pack()
-
-    def plot_frame(self, t):
-        """Get a plot the frame"""
-        t0 = np.float64(t)
-        it = np.argmin(abs(self.data['tframes'] - t0))
-        dummy = self.data['frames'][:, :, it].squeeze()
-        self.image.set_data(dummy)
-        self.canvas.draw()
