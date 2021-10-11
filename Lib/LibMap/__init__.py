d612046696c2b794097f01ef3d8836f9a66bd7e4
ruejo <ruejo@ipp.mpg.de>
2021-10-11 11:49:29 +0200

    small chang in __init__

--- Lib/LibMap/__init__.py
+++ Lib/LibMap/__init__.py
@@ -1,3 +1,6 @@
+from Lib.LibMap.Calibration import CalParams, CalibrationDatabase
 from Lib.LibMap.StrikeMap import StrikeMap
 from Lib.LibMap.Common import *
-from Lib.LibMap.Calibration import CalParams, CalibrationDatabase
+from Lib.LibMap.FILD import remap_all_loaded_frames as remap_all_loaded_frames_FILD
+from Lib.LibMap.Scintillator import Scintillator
+from Lib.LibMap.StrikeMap import StrikeMap
