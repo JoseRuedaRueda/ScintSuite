b31f419fc6555189934f2847f407113b47097091
Pablo Oyola <pablo.oyola@ipp.mpg.de>
2021-12-03 12:48:34 +0100

    Working GUI for plotting the beam geometry in the poloidal plane

--- Examples/Others/ihibp_beam_gui.py
+++ Examples/Others/ihibp_beam_gui.py
@@ -2,13 +2,14 @@
 Script to launch the iHIBP gui.
 """
 
-import tkinter as tk 
-from Lib.GUIs.BEP_gui import iHIBP_beam
+import tkinter as tk
+from Lib.GUIs.iHIBP_beam import appHIBP_beam
 from Lib.LibParameters import iHIBP
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.resizable(height=None, width=None)
-    a = iHIBP_beam(root, origin=iHIBP['port_center'])
+    a = appHIBP_beam(root, origin=iHIBP['port_center'],
+                     shotnumber=39810, time=0.50)
     root.mainloop()
     root.destroy()
