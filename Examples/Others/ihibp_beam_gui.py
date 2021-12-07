99c08b9f0844dd4053c8b67d1414dbc8514c2d1d
Pablo Oyola <pablo.oyola@ipp.mpg.de>
2021-12-07 11:43:19 +0100

    Minor error correction. Changelog updated.

--- Examples/Others/ihibp_beam_gui.py
+++ Examples/Others/ihibp_beam_gui.py
@@ -3,10 +3,11 @@ Script to launch the iHIBP gui.
 """
 
 import tkinter as tk
+import Lib
 from Lib.GUIs.iHIBP_beam import appHIBP_beam
-from Lib.LibParameters import iHIBP
 
 if __name__ == '__main__':
+    iHIBP = Lib.dat.iHIBP
     root = tk.Tk()
     root.resizable(height=None, width=None)
     a = appHIBP_beam(root, origin=iHIBP['port_center'],
