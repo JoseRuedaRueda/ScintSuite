c724bd367d219e55723ede1fe29689110f337196
Javier Hidalgo Salaverri <javier.hidalgo.salaverri@gmailc.om>
2022-02-17 16:52:54 +0100

    Added FILD4 object

--- Lib/LibPaths.py
+++ Lib/LibPaths.py
@@ -45,7 +45,7 @@ class Path:
             self.bcoils_phase_corr = os.path.join(self.ScintSuite,
                                                   'Data',
                                                   'Magnetics')
-            self.FILD4_trayectories = \
+            self.FILD4_trajectories = \
                 '/afs/ipp/home/j/javih/FILD4/'
             self.fonts = [
                 '/usr/share/fonts/truetype',
