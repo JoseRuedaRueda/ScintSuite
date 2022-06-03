29a3f1420905a6c8f101e0c3cc6a927e1d33c954
Jose Rueda Rueda <jose.rueda.rueda@ipp.mpg.de>
2022-06-03 18:04:03 +0200

    San Jacobo prerelease

--- Lib/_Mapping/__init__.py
+++ Lib/_Mapping/__init__.py
@@ -1,5 +1,5 @@
 from Lib._Mapping._Calibration import CalParams, CalibrationDatabase
-from Lib._Mapping.StrikeMap import StrikeMap
+from Lib._Mapping._StrikeMap import StrikeMap
 from Lib._Mapping._Common import *
 from Lib._Mapping._FILD import *
 from Lib._Mapping._INPA import remapAllLoadedFrames as remapAllLoadedFramesINPA
