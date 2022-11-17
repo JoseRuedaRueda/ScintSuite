689bb939c6829c6f7200fceca5c30a72e95aae08
Hannah Lindl <hannah.lindl@ipp.mpg.de>
2022-11-17 10:45:14 +0100

    mp4 video independent of input

--- Lib/_Video/_MP4files.py
+++ Lib/_Video/_MP4files.py
@@ -6,40 +6,34 @@ Written by Hannah Lindl: hannah.lindl@ipp.mpg.de
 """
 import numpy as np
 import subprocess as sp
+import ffmpeg
 
 
-def read_file(video, filename_video: str):
+def read_file(filename_video: str):
     """
     load greyscale camera data with ffmpeg
-    @param video: video properties containing the camera resolution and the timebase 
-    @param filename_video: path/filename of the camera data 
+    @param video: video properties containing the camera resolution and the timebase
+    @param filename_video: path/filename of the camera data
     """
-
-    time = video.timecal
-    width = video.properties['width']
-    height = video.properties['height']
-    nf = video.nf
-    
-    initial_time = 0
-
 
     FFMPEG_BIN = 'ffmpeg'
     command = [FFMPEG_BIN,
                  '-loglevel', 'error',
                  '-hide_banner',
-                '-ss', str(initial_time),
                '-i', filename_video,
-                '-frames:v', str(nf),
                '-f', 'image2pipe',
                 '-pix_fmt', 'gray16le',
                '-vcodec', 'rawvideo', '-']
 
+    width = int(ffmpeg.probe(filename_video)["streams"][0]['width'])
+    height = int(ffmpeg.probe(filename_video)["streams"][0]['height'])
+    fps = int(ffmpeg.probe(filename_video)["streams"][0]['avg_frame_rate'][:-2])
+    nf = int(ffmpeg.probe(filename_video)['streams'][0]['nb_frames'])
     pipe = sp.Popen(command,stdout = sp.PIPE, bufsize = 10**9)
     raw_image = pipe.stdout.read(nf*2*width*height)
     image=np.frombuffer(raw_image, np.uint16).reshape([-1, height, width])
     pipe.stdout.flush()
 
-
     frames = (image.astype(float))
 
-    return {'nf': video.nf, 'nx': width, 'ny': height, 'frames': frames, 'tframes': time}
+    return {'nf': nf, 'width': width, 'height': height, 'frames': frames, 'fps': fps}
