c2ecbc8b64ab5f4968f14be23e505b33fd83d344
Lina Velarde <lvelarde@us.es>
2022-12-03 10:34:52 +0000

    Update netcdf4 files v2

--- Lib/_Video/_NetCDF4files.py
+++ Lib/_Video/_NetCDF4files.py
@@ -49,14 +49,14 @@ def read_file_anddata(filename):
     frames = {'nf': vid.shape[2],
             'width': vid.shape[0], 
             'height': vid.shape[1], 
-            'frames': vid
-            'timebase': timebase}}
+            'frames': vid,
+            'timebase': timebase}
 
     imageheader = {
         'biWidth': vid.shape[0],
         'biHeight': vid.shape[1],
         'framesDtype': vid.dtype}
-    header = {'ImageCount': vid.shape[2]]}
+    header = {'ImageCount': vid.shape[2]}
     # Possible bytes per pixels for the camera
     BPP = {'uint8': 8, 'uint16': 16, 'uint32': 32, 'int32': 32, 'uint64': 64}
     settings = {
@@ -73,58 +73,6 @@ def read_file_anddata(filename):
         raise Exception('Expected uint8,16,32,64 in the frames')
  
     return frames, header, imageheader, settings
-
-
-
-def read_data(filename):
-    """
-
-    Lina Velarde: lvelarde@us.es
-
-    Return a series of dictionaries, with all the info we can extract from the XIMEA files.
-
-    :param filename
-
-    @return image_header: dictionary containing the info about the image size,
-    and shape
-    @return header: Header 'similar' to the case of a cin file
-    @return settings: dictionary similar to the case of the cin file (it only
-    contains the exposition time)
-    """
-    # Look for a png to extract the file and a .txt for the time information
-    # f = []
-    look_for_nc = True
-    counter = 0
-    for file in os.listdir(path):
-        # if file.endswith('.txt'):
-        #     f.append(os.path.join(path, file))
-        if file.endswith('.nc'):
-            if look_for_nc:
-                data = nc.Dataset(os.path.join(path, file), mode='r')
-                si = data['frames'].shape
-                imageheader = {
-                    'biWidth': si[0],
-                    'biHeight': si[1],
-                    'framesDtype': data['frames'].dtype}
-                look_for_nc = False
-    # If no nc was found, raise and error
-    header = {'ImageCount': si[2]}
-    if look_for_nc:
-        print('No .nc files in the folder...')
-        return 0, 0, 0, 0
-    # Possible bytes per pixels for the camera
-    BPP = {'uint8': 8, 'uint16': 16, 'uint32': 32, 'uint64': 64}
-    settings = {}
-    try:
-        settings['RealBPP'] = BPP[imageheader['framesDtype'].name]
-        text = 'In the nc there is no info about the real BitesPerPixel'\
-            + ' used in the camera. Assumed that the BPP coincides with'\
-            + ' the byte size of the variable!!!'
-        print(text)
-    except KeyError:
-        raise Exception('Expected uint8,16,32,64 in the frames')
- 
-    return header, imageheader, settings
 
 
 def load_nc(filename: str, frame_number: int = None):
