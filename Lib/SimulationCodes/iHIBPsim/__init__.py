a4de812120b0c902fc829d193ea60b38060a5c25
Pablo Oyola <pablo.oyola@ipp.mpg.de>
2023-02-23 15:44:12 +0100

    Uploaded bayesian noise removal

--- Lib/SimulationCodes/iHIBPsim/__init__.py
+++ Lib/SimulationCodes/iHIBPsim/__init__.py
@@ -3,15 +3,16 @@ Interaction with the iHIBPsim code
 
 Pablo Oyola - pablo.oyola@ipp.mog.de
 """
-from Lib.SimulationCodes.Common.fields import fields as ihibpfields
-from Lib.SimulationCodes.iHIBPsim.profiles import ihibpProfiles
-import Lib.SimulationCodes.iHIBPsim.crossSections as xsection
-import Lib.SimulationCodes.iHIBPsim.orbits as orbs
-import Lib.SimulationCodes.iHIBPsim.attenuation as att
-import Lib.SimulationCodes.iHIBPsim.strikelib as strikes
-import Lib.SimulationCodes.iHIBPsim.strikeline as strline
-import Lib.SimulationCodes.iHIBPsim.execute as exe
-import Lib.SimulationCodes.iHIBPsim.nml as namelist
-import Lib.SimulationCodes.iHIBPsim.geom as geom
-import Lib.SimulationCodes.iHIBPsim.hibp_utils as utils
-import Lib.SimulationCodes.iHIBPsim.deposition as depos
+# from Lib.SimulationCodes.Common.fields import fields as ihibpfields
+# from Lib.SimulationCodes.iHIBPsim.profiles import ihibpProfiles
+# import Lib.SimulationCodes.iHIBPsim.crossSections as xsection
+# import Lib.SimulationCodes.iHIBPsim.orbits as orbs
+# import Lib.SimulationCodes.iHIBPsim.orbits_old as orbs_old
+# import Lib.SimulationCodes.iHIBPsim.attenuation as att
+# import Lib.SimulationCodes.iHIBPsim.strikelib as strikes
+# import Lib.SimulationCodes.iHIBPsim.strikeline as strline
+# import Lib.SimulationCodes.iHIBPsim.execute as exe
+# import Lib.SimulationCodes.iHIBPsim.nml as namelist
+# import Lib.SimulationCodes.iHIBPsim.geom as geom
+# import Lib.SimulationCodes.iHIBPsim.hibp_utils as utils
+# import Lib.SimulationCodes.iHIBPsim.deposition as depos
