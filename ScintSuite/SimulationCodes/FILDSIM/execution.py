19c15a11b7e97c3886929078f4c6f771806a57e4
alrevi <alrevi@toki01.ipp.mpg.de>
2025-09-17 16:10:44 +0200

    minor changes

--- ScintSuite/SimulationCodes/FILDSIM/execution.py
+++ ScintSuite/SimulationCodes/FILDSIM/execution.py
@@ -330,8 +330,9 @@ def get_energy(gyroradius, B: float, A: float = 2.01410178, Z: float = 1.0):
 
     :return E: the energy [in eV]
     """
+    # E[ev] = (1/2 * (r[cm]/100*Z*B[T])**2 / m) * ec
     m = ssp.amu2kg * A  # Mass of the ion
-    E = 0.5 * (gyroradius/100.0 * Z * B)**2 / m * ssp.ec
+    E = (0.5 * ((gyroradius/100.0) * Z * B)**2 / m) * ssp.ec
     return E
 
 
@@ -348,6 +349,35 @@ def get_gyroradius(E, B: float, A: float = 2.01410178, Z: float = 1.0):
 
     :return r: Larmor radius as taken from FILD strike map [in cm]
     """
+    # r[cm] = sqrt(2*(E[eV]/ec)*m) / (Z*B[T]) * 100
     m = ssp.amu2kg * A  # Mass of the ion
-    r = 100. * np.sqrt(2.0 * E * m / ssp.ec) / Z / B
+    r = 100. * np.sqrt(2.0 * (E/ssp.ec) * m) / (Z*B)
     return r
+
+
+def get_velocity(*, gyroradius = None, E=None, 
+                 B: float = 1.9, A: float = 2.01410178, Z: float = 1.0):
+    """
+    Calculate the velocity given a gyroradius or energy, FILDSIM criteria
+
+    Alex Reyner: alereyvinn@alum.us.es
+
+    :param  gyroradius: Larmor radius as taken from FILD strike map [in cm]
+    :param  energy: Energy [eV]
+    :param  B: Magnetic field, [in T]
+    :param  A: Ion mass number
+    :param  Z: Ion charge [in e units]
+
+    :return E: the energy [in eV]
+    """
+
+    if gyroradius is not None:
+        # v = ((r[cm]/100) * Z*B[T] * ec ) / m
+        m = ssp.amu2kg * A  # Mass of the ion
+        v_tot = ((gyroradius/100) *Z*B* ssp.ec)/m
+    elif gyroradius is None and E is not None:
+        # v = sqrt(2*E[eV]*ec / m)
+        m = ssp.amu2kg * A  # Mass of the ion
+        v_tot = np.sqrt(2 * (E*ssp.ec) / m)
+
+    return v_tot
--- ScintSuite/_Plotting/_ColorMaps.py
+++ ScintSuite/_Plotting/_ColorMaps.py
@@ -5,26 +5,60 @@ Jose Rueda Rueda: jrrueda@us.es
 
 Contains:
     -Gamma_II: Similar to IDL colormap with the same name
+    -Gamma_III: Same as Gamma_II, but with grey at the bottom for contrast
     -Cai: Color map with the colors of Cadiz
 """
 from matplotlib.colors import LinearSegmentedColormap
-__all__ = ['Gamma_II', 'Cai']
+__all__ = ['Gamma_II', 'Gamma_III', 'Cai']
 
+# Deprecated
+# def Gamma_II(n=256):
+#     """
+#     Gamma II colormap
+
+#     This function creates the colormap that coincides with the
+#     Gamma_II_colormap of IDL.
+
+#     Jose Rueda: jrrueda@us.es
+
+#     :param  n: numbers of levels of the output colormap
+#     """
+#     cmap = LinearSegmentedColormap.from_list(
+#         'mycmap', ['black', 'blue', 'red', 'yellow', 'white'], N=n)
+#     return cmap
 
 def Gamma_II(n=256):
     """
-    Gamma II colormap
+    Gamma_II colormap
 
-    This function creates the colormap that coincides with the
-    Gamma_II_colormap of IDL.
+    Creates the colormap very similar with the Gamma_II of IDL
 
-    Jose Rueda: jrrueda@us.es
+    Alex Reyner: alereyvinn@alum.us.es
 
     :param  n: numbers of levels of the output colormap
     """
     cmap = LinearSegmentedColormap.from_list(
-        'mycmap', ['black', 'blue', 'red', 'yellow', 'white'], N=n)
+        'mycmap', ['black', 'blue', 'purple', 
+                   'red', 'orange', 'yellow', 'white'], N=n)
     return cmap
+
+
+def Gamma_III(n=256):
+    """
+    Gamma_II colormap with extra color in the lower range for higher contrast
+
+    Alex Reyner: alereyvinn@alum.us.es
+
+    :param  n: numbers of levels of the output colormap
+    """
+    color_positions = [0.0, 1/6/2, 1/6*1, 1/6*2, 1/6*3, 1/6*4, 1/6*5, 1]
+    colors = ['black', 'silver', 'blue', 'purple', 
+              'red', 'orange', 'yellow', 'white']
+
+    cmap = LinearSegmentedColormap.from_list(
+        "mycmap", list(zip(color_positions, colors)),N=n)
+    return cmap
+
 
 
 def Cai(n=256):
