4feb490c9f5d068b5f55f617788dcbbab3213eff
Jose <jruedaru@uci.edu>
2026-03-11 14:14:41 -0700

    test7

--- docs/conf.py
+++ docs/conf.py
@@ -25,31 +25,6 @@ autodoc_typehints = 'description'
 # Autosummary: generate stub pages for modules/classes/functions
 autosummary_generate = True
 
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'ScintSuite'
 copyright = '2025, Jose Rueda, Pablo Oyola, Lina Velarde, Javier Hidalgo'
 author = 'Jose Rueda, Pablo Oyola, Lina Velarde, Javier Hidalgo'
 release = '1.4.3'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
