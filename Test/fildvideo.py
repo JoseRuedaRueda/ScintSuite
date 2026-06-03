f0b822cad9a518c6570fa2c527e3b872f60b945f
Theo Gheorghiu <tgheorghiu@btinternet.com>
2026-06-03 13:05:00 +0100

    misc alts

--- Test/fildvideo.py
+++ Test/fildvideo.py
@@ -3,7 +3,10 @@
 import numpy as np
 import pyuda
 import netCDF4 as nc
+import ScintSuite._Video._NetCDF4files as ncdf
+import matplotlib.pyplot as plt
 from rich import inspect
+
 
 ## import os and disable file locking
 import os
@@ -12,13 +15,13 @@ os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
 ## activate the client
 client = pyuda.Client()
 
-path = "$MAST_DATA/054/54287/LATEST/xfx054287.nc"
+path = "$MAST_DATA/054/54277/LATEST/xfx054277.nc"
 
 ## directories list 
-directories_list = client.list_archive_directories("$MAST_DATA/054/54287/")
+directories_list = client.list_archive_directories("$MAST_DATA/054/54277/")
 
 ## file list
-file_list = client.list_archive_files("$MAST_DATA/054/54287/LATEST")
+file_list = client.list_archive_files("$MAST_DATA/054/54277/LATEST")
 
 ## signal names 
 signal_names = client.list_file_signals(path)
@@ -27,3 +30,13 @@ signal_names = client.list_file_signals(path)
 client.get("/xfx/video", path)
 ## or this also works (equivalent)
 client.get("/xfx/video", 54287)
+
+
+vvideo = ncdf.client.get("/xfx/video",path)
+vtime = ncdf.client.get("/xfx/time",path)
+vfps = ncdf.client.get("/devices/fps",path)
+vexposure = ncdf.client.get("/devices/exposure",path)
+vrfild = ncdf.client.get("/devices/RFILD",path)
+vfildangle = ncdf.client.get("/devices/FILDangle",path)
+vanaloguegain = ncdf.client.get("/devices/analoggain",path)
+vdigitalgain = ncdf.client.get("/devices/diggain",path)
