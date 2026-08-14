fix resolution calculation

ScintSuite/_StrikeMap/_FILD_INPA_ParentStrikeMap.py
@@ -273,15 +273,12 @@ class FILDINPA_Smap(GeneralStrikeMap):
         namex = 'remap_' + variables[0]
         namey = 'remap_' + variables[1]
         # Get the physical units
-        unitsx = self.strike_points.header['info'][namex]['units']
-        unitsy = self.strike_points.header['info'][namey]['units']
+        unitsx = self.strike_points.df.attrs['units'][namex]
+        unitsy = self.strike_points.df.attrs['units'][namey]
         # first check if the remapped strike points are there
-        if (namex not in self.strike_points.header['info'].keys()
-                or namey not in self.strike_points.header['info'].keys()):
+        if (namex not in self.strike_points.df.keys()
+                or namey not in self.strike_points.df.keys()):
             raise Exception('Non remap data in the strike points object!')
-        else:
-            iix = self.strike_points.header['info'][namex]['i']
-            iiy = self.strike_points.header['info'][namey]['i']
         # Get the names of the variables to fit
         xModel = parseModelNames(diag_options['x_method'])
         yModel = parseModelNames(diag_options['y_method'])
@@ -315,33 +312,34 @@ class FILDINPA_Smap(GeneralStrikeMap):
         for ix in tqdm(range(nx), disable=tqdm_disable):
             for iy in range(ny):
                 # -- Select the data:
-                data = self.strike_points.data[ix, iy]
+                datax = self.strike_points.get_from_df(namex, XI_index=ix, gyroradius_index=iy)
+                datay = self.strike_points.get_from_df(namey, XI_index=ix, gyroradius_index=iy)
                 # if there is no enough data, skip this point
-                if self.strike_points.header['counters'][ix, iy] < min_statistics:
+                if len(datax) < min_statistics:
                     continue
                 # -- Prepare the basic bin edges
                 # Prepare the bin edges according to the desired width                    
                 if adaptative:
-                    sigmax = np.std(data[:, iix])
+                    sigmax = np.std(datax)
                     dx = sigmax / float(bin_per_sigma)
-                    sigmay = np.std(data[:, iiy])
+                    sigmay = np.std(datay)
                     dy = sigmay / float(bin_per_sigma)
                 else:
                     dx = diag_options['dx']
                     dy = diag_options['dy']
                 xedges = \
-                    np.arange(start=data[:, iix].min() - dx,
-                              stop=data[:, iix].max() + dx,
+                    np.arange(start=datax.min() - dx,
+                              stop=datax.max() + dx,
                               step=dx)
                 yedges = \
-                    np.arange(start=data[:, iiy].min() - dy,
-                              stop=data[:, iiy].max() + dy,
+                    np.arange(start=datay.min() - dy,
+                              stop=datay.max() + dy,
                               step=dy)
                 # -- fit the x variable
                 params, self._resolutions['fits_' + variables[0]][ix, iy], \
                     self._resolutions['norm_' + variables[0]][ix, iy], unc = \
                     _fit_to_model_(
-                        data[:, iix], bins=xedges,
+                        datax, bins=xedges,
                         model=diag_options['x_method'],
                         confidence_level=confidence_level,
                         uncertainties=calculate_uncertainties)
@@ -356,7 +354,7 @@ class FILDINPA_Smap(GeneralStrikeMap):
                 params, self._resolutions['fits_' + variables[1]][ix, iy], \
                     self._resolutions['norm_' + variables[1]][ix, iy], unc = \
                     _fit_to_model_(
-                        data[:, iiy], bins=yedges,
+                        datay, bins=yedges,
                         model=diag_options['y_method'],
                         confidence_level=confidence_level,
                         uncertainties=calculate_uncertainties)
@@ -652,14 +650,9 @@ class FILDINPA_Smap(GeneralStrikeMap):
         # Get the shape of the map
         nx, ny = self.shape
         # Get the index of the colums containing the scintillation position
-        if not remap_in_pixel_space:
-            ix1 = self.strike_points.header['info']['x1']['i']
-            ix2 = self.strike_points.header['info']['x2']['i']
-        else:
-            ix1 = self.strike_points.header['info']['xcam']['i']
-            ix2 = self.strike_points.header['info']['ycam']['i']
         # Loop over the deseired variables
         var_list = [k for k in self._map_interpolators.keys() if k.endswith('pix')==False]
+        camera = True if 'xcam' in self.strike_points.df.keys() else False
         for k in var_list:
             # See if we need to overwrite
             name = 'remap_' + k
@@ -669,48 +662,26 @@ class FILDINPA_Smap(GeneralStrikeMap):
             else:
                 interpolator = k
             was_there = False
-            if name in self.strike_points.header['info'].keys():
+            if name in self.strike_points.df.keys():
                 was_there = True
                 if overwrite:
                     logger.warning('%s found in the object, overwritting' % k)
-                    ivar = self.strike_points.header['info'][name]['i']
                 else:
                     logger.info('%s found in the object, skipping' % k)
                     continue
             # Loop over the strike points pairs
-            for ix in range(nx):
-                for iy in range(ny):
-                    if self.strike_points.header['counters'][ix, iy] > 0:
-                        n_strikes = \
-                            self.strike_points.header['counters'][ix, iy]
-                        remap_data = np.zeros((n_strikes, 1))
-                        remap_data[:, 0] = \
-                            self._map_interpolators[interpolator](
-                                self.strike_points.data[ix, iy][:, [ix1, ix2]])
-                        # self.strike_points.data[ip, ir][:, iiy])
-                        # append the remapped data to the object
-                        if was_there:
-                            self.strike_points.data[ix, iy][:, ivar] = \
-                                remap_data
-                        else:
-                            self.strike_points.data[ix, iy] = \
-                                np.append(self.strike_points.data[ix, iy],
-                                          remap_data, axis=1)
-            # Update the headers, if needed
-            if not was_there:
-                Old_number_colums = len(self.strike_points.header['info'])
-                # Take the original variable as base for the dictionary
-                extra_column = dict.fromkeys([name, ])
-                extra_column[name] = {
-                    'i': Old_number_colums,
-                    'units': '@Todo',
-                    'lonName': name,
-                    'shortName': name
-                }
-                extra_column[name]['i'] = Old_number_colums
-                # Update the header
-                self.strike_points.header['info'].update(extra_column)
-
+            if k.endswith('pix') and camera:
+                self.strike_points.df[name] = self._map_interpolators[k]((self.strike_points(('xcam','ycam')).value))
+            elif k.endswith('pix') and not camera:
+                logger.warning('Cannot remap %s in pixel space, no camera data' % k)
+            else:
+                self.strike_points.df[name] = self._map_interpolators[k]((self.strike_points(('x1','x2')).value))
+            
+            # Update the headers and info
+            self.strike_points.df.attrs['units'][name] = '@Todo'
+            self.strike_points.df.attrs['longName'][name] = name
+            self.strike_points.df.attrs['shortName'][name] = name
+            
     def remap_external_strike_points(self, strikes, overwrite: bool = True):
         """
         Remap the signal (or any external) StrikePoints
@@ -746,7 +717,7 @@ class FILDINPA_Smap(GeneralStrikeMap):
                     logger.info('%s found in the object, skipping' % k)
                     continue
             # Loop over the strike points pairs
-            if k.endswith('pix') and not camera:
+            if k.endswith('pix') and camera:
                 strikes.df[name] = self._map_interpolators[k]((strikes(('xcam','ycam')).value))
             else:
                 strikes.df[name] = self._map_interpolators[k]((strikes(('x1','x2')).value))

ScintSuite/_StrikeMap/_INPA_StrikeMap.py
@@ -173,6 +173,7 @@ class Ismap(FILDINPA_Smap):
                                     B: float = 1.8,
                                     Z: float = 1.0,
                                     A: float = 2.01410,
+                                    fitSplines: bool = False
                                     ) -> None:
         """
         Build the INPA weight function.
@@ -246,13 +247,7 @@ class Ismap(FILDINPA_Smap):
         Tmatrix = self._grid_interp['transformation_matrix'][nameT]
         gridT = self._grid_interp['transformation_matrix'][nameT+'_grid']
         camera_frame = Tmatrix.shape[2:4]
-        # --- Get the index of the different colums
-        jpx = self.secondaryStrikes.header['info']['ycam']['i']
-        jpy = self.secondaryStrikes.header['info']['xcam']['i']
-        jX = self.secondaryStrikes.header['info'][variablesFI[0]]['i']
-        jY = self.secondaryStrikes.header['info'][variablesFI[1]]['i']
-        jkind = self.secondaryStrikes.header['info']['kind']['i']
-        jw = self.secondaryStrikes.header['info'][weight]['i']
+
         # Block 1: Preparation phase -------------------------------------------
         # --- Prepare the grids
         # - Edges
@@ -274,13 +269,13 @@ class Ismap(FILDINPA_Smap):
         nStrikes = self.secondaryStrikes.data[0, 0].shape[0]
         dummy = np.zeros((nStrikes, 5))
 
-        dummy[:, 0] = self.secondaryStrikes.data[0, 0][:, jpx]
-        dummy[:, 1] = self.secondaryStrikes.data[0, 0][:, jpy]
-        dummy[:, 2] = self.secondaryStrikes.data[0, 0][:, jX]
-        dummy[:, 3] = self.secondaryStrikes.data[0, 0][:, jY]
-        dummy[:, 4] = self.secondaryStrikes.data[0, 0][:, jkind]
+        dummy[:, 0] = self.secondaryStrikes('ycam').value
+        dummy[:, 1] = self.secondaryStrikes('xcam').value
+        dummy[:, 2] = self.secondaryStrikes(variablesFI[0]).value
+        dummy[:, 3] = self.secondaryStrikes(variablesFI[1]).value
+        dummy[:, 4] = self.secondaryStrikes('kind').value
         if weight is not None:
-            w = self.secondaryStrikes.data[0, 0][:, jw]
+            w = self.secondaryStrikes(weight).value
         else:
             w = np.ones(nStrikes)
 
@@ -293,6 +288,8 @@ class Ismap(FILDINPA_Smap):
                 bins=edges,
                 weights=w,
         )
+        logger.info('Total weight of the histogram: %f', H.sum())
+        logger.info('Total weight of the original strikes: %f', w.sum())
         # check there is nothing negative
         totalWeightBeforeOptics = H.sum()
         # Add the finite focus of the optics
@@ -311,7 +308,7 @@ class Ismap(FILDINPA_Smap):
             deltaWeight = abs(totalWeightAfterOptics-totalWeightBeforeOptics)\
                 / totalWeightBeforeOptics
             logger.debug('Change of weight due to optics: %f ' % deltaWeight)
-            if deltaWeight > 0.001:
+            if deltaWeight > 0.01:
                 logger.error('Total Wegith before the optic focus: %f',
                              totalWeightBeforeOptics)
                 logger.error('Total Wegith after the optic focus: %f',
@@ -411,40 +408,43 @@ class Ismap(FILDINPA_Smap):
         self.instrument_function.attrs['A'] = A
 
         # ---- Perform fitting
-        logger.info('Fitting to bivariant Normal distributions')
-        # Allocate a copy
-        self.instrument_function_fit = self.instrument_function.copy()
-        self.instrument_function_fit.attrs['Info'] = \
-            'Fitted to a BivariateNormal'
-        # Prepare the model
-        model = ssmodels.BivariateNormalDistribution
-        # Prepare grids
-        XX, YY = np.meshgrid(self.instrument_function.x,
-                             self.instrument_function.y,
-                             indexing='ij')
-        # Perfom the loop
-        for jk in range(self.instrument_function.kind.size):
-            if self.instrument_function.isel(kind=jk).sum() < 1.0e-8:  # So zero
-                continue
-            # Fit a bivariant spline for each scintilaltor position
-            for ixs in range(self.instrument_function.xs.size):
-                for iys in range(self.instrument_function.ys.size):
-                    matrix = self.instrument_function.isel(kind=jk,
-                                                           xs=ixs,
-                                                           ys=iys).values.copy()
-                    sumMatrix = matrix.sum()
-                    if sumMatrix < 1.0e-8:
-                        continue
-                    matrix /= sumMatrix
-                    try:
-                        params = ssmodels.guessParamsBivariateNormalDistribution(
-                            XX.flatten(), YY.flatten(), matrix.flatten())
-                        result = model.fit(data=matrix.flatten(),
-                                        params=params, x=XX.flatten(),
-                                        y=YY.flatten())
-                        self.instrument_function_fit.values[ixs, iys, :, :, jk] = \
-                            result.eval(x=XX.flatten(), y=YY.flatten()).reshape(
-                                XX.shape) * sumMatrix
-                    except:
-                        self.instrument_function_fit.values[ixs, iys, :, :, jk] =0.0
+        if fitSplines:
+            logger.info('Fitting to bivariant Normal distributions')
+            # Allocate a copy
+            self.instrument_function_fit = self.instrument_function.copy()
+            self.instrument_function_fit.attrs['Info'] = \
+                'Fitted to a BivariateNormal'
+            # Prepare the model
+            model = ssmodels.BivariateNormalDistribution
+            # Prepare grids
+            XX, YY = np.meshgrid(self.instrument_function.x,
+                                self.instrument_function.y,
+                                indexing='ij')
+            # Perfom the loop
+            for jk in range(self.instrument_function.kind.size):
+                if self.instrument_function.isel(kind=jk).sum() < 1.0e-8:  # So zero
+                    continue
+                # Fit a bivariant spline for each scintilaltor position
+                for ixs in range(self.instrument_function.xs.size):
+                    for iys in range(self.instrument_function.ys.size):
+                        matrix = self.instrument_function.isel(kind=jk,
+                                                            xs=ixs,
+                                                            ys=iys).values.copy()
+                        sumMatrix = matrix.sum()
+                        if sumMatrix < 1.0e-8:
+                            continue
+                        matrix /= sumMatrix
+                        try:
+                            params = ssmodels.guessParamsBivariateNormalDistribution(
+                                XX.flatten(), YY.flatten(), matrix.flatten())
+                            result = model.fit(data=matrix.flatten(),
+                                            params=params, x=XX.flatten(),
+                                            y=YY.flatten())
+                            self.instrument_function_fit.values[ixs, iys, :, :, jk] = \
+                                result.eval(x=XX.flatten(), y=YY.flatten()).reshape(
+                                    XX.shape) * sumMatrix
+                        except:
+                            self.instrument_function_fit.values[ixs, iys, :, :, jk] =0.0
+        else:
+            self.instrument_function_fit = None
         return

ScintSuite/_TimeTrace/_timetrace.py
@@ -57,7 +57,8 @@ def trace(frames, mask):
     max_of_roi = np.zeros((nt,), dtype='float64')
     
     nmask_valid = frames[mask, 0].size
-    
+    if nmask_valid == 0:
+        raise Exception('The mask is empty, no pixels to calculate the trace')
     for it in range(nt):
         sum_of_roi[it]  = frames[mask, it].astype('float64').sum()
         mean_of_roi[it] = sum_of_roi[it] / nmask_valid

ScintSuite/_Tomography/_main_class.py
@@ -513,7 +513,7 @@ class Tomography():
         self.inversion['cimmino']['time'].attrs['long_name'] = '$t (s)$'
 
 
-    def tikhonov0(self, alpha, weights=None, **kargs) -> None:
+    def tikhonov0(self, alpha, weights=None, nsmooth=0, **kargs) -> None:
         """
         Perform a 0th order Tikonov regularized regression
 
@@ -523,7 +523,10 @@ class Tomography():
             a list or array. In this latter case, the regression will be done
             for each value in the list (array)
         :param  weights: weights, placeholder for the future
+        :param  nsmooth: size of the smoothing window, if > 0 the solution is smoothed with a median filter. This is higly not recomended as it is not going to conserve the total number of EP, this is just a quick 
+        and dirty hack to get something usable during exploratory scans
         :param  **kargs: extra arguments to initialise skitlearn ridge regressor
+        
         """
         # --- Ensure we have an array or iterable:
         if isinstance(alpha, (list, np.ndarray)):
@@ -562,12 +565,20 @@ class Tomography():
         # --- Save it in the dataset
         self.inversion['tikhonov0'] = xr.Dataset()
         if self.Wndims == 4:
+            if nsmooth > 0:
+                from scipy.ndimage import median_filter
+                for i in range(n_alpha):
+                    beta_shaped[..., i] = median_filter(beta_shaped[..., i], size=nsmooth)
             self.inversion['tikhonov0']['F'] = xr.DataArray(
                     beta_shaped, dims=('x', 'y', 'alpha'),
                     coords={'x': self.W['x'], 'y': self.W['y'],
                             'alpha': alp}
             )
         else:
+            if nsmooth > 0:
+                from scipy.ndimage import median_filter
+                for i in range(n_alpha):
+                    beta_shaped[..., i] = median_filter(beta_shaped[..., i], size=nsmooth)
             self.inversion['tikhonov0']['F'] = xr.DataArray(
                     beta_shaped, dims=('x', 'y', 'z', 'alpha'),
                     coords={'x': self.W['x'], 'y': self.W['y'], 'z': self.W['z'],
@@ -1248,9 +1259,10 @@ class Tomography():
                 ax[1, 1].set_title('alpha: %.2e' % Eprof.alpha[ialpha])
             lineE.set_ydata(E2plot)
             lineR.set_ydata(R2plot)
-            pointRes.set_xdata(MSE.sel(method='nearest',
-                               **slider_vars_val).values)
-            pointRes.set_ydata(Total.sel(method='nearest',**slider_vars_val).values)
+
+            pointRes.set_xdata(np.atleast_1d(MSE.sel(method='nearest',
+                               **slider_vars_val).values))
+            pointRes.set_ydata(np.atleast_1d(Total.sel(method='nearest',**slider_vars_val).values))
 
             img.set_data(data.sel(method='nearest',**slider_vars_val ).values.T)
             